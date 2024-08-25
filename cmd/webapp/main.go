cb7c38cd362b83d41548ccffa1bb36fe899d2560
Pablo Bastidas <pablobastidasv@gmail.com>
2024-08-25 09:19:09 +0200

    adding logs and usage of ENV environment variable

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -12,44 +12,76 @@ import (
 	"github.com/pablobastidasv/fridge_inventory/internals/inventorymanager"
 	"github.com/pablobastidasv/fridge_inventory/server"
 	"github.com/pablobastidasv/fridge_inventory/storage/postgres"
+	slogecho "github.com/samber/slog-echo"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		slog.Warn(".env file not present")
-	}
+	_ = godotenv.Load()
+
+	// creating custom log
+	logger := newLogger()
+
+	// Loading database
+	db := newDb()
+	defer db.Close()
 
 	e := echo.New()
 
 	e.Static("/statics", "assets")
 
-	// Middleware
-	e.Use(middleware.Logger())
+	// Middlewares
+	e.Use(slogecho.New(logger))
 	e.Use(middleware.Recover())
-
-	connStr := os.Getenv("DBSTRING") //"postgresql://postgres:password@localhost:54321/postgres?sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
 
 	store := postgres.New(db)
 	manager := inventorymanager.New(store)
 
-    e.GET("/", handler.GetMainIndex())
+	e.GET("/", handler.GetMainIndex())
 
-    invGroup := e.Group("/inventories")
-    invGroup.GET("", handler.GetInventoryItems(manager))
-    invGroup.PUT("/:id", handler.PutInventory(manager))
-    invGroup.GET("/:id/edit", handler.GetInventoryForm(manager))
+	invGroup := e.Group("/inventories")
+	invGroup.GET("", handler.GetInventoryItems(manager))
+	invGroup.PUT("/:id", handler.PutInventory(manager))
+	invGroup.GET("/:id/edit", handler.GetInventoryForm(manager))
 
-    prdGroup := e.Group("/products")
+	prdGroup := e.Group("/products")
 	prdGroup.GET("", handler.GetProducts(manager))
 	prdGroup.POST("", handler.PostProducts(manager), server.WithTransaction)
 	prdGroup.GET("/new", handler.GetProductsForm(manager))
-    prdGroup.DELETE("/:id", handler.DeleteProduct(manager), server.WithTransaction)
+	prdGroup.DELETE("/:id", handler.DeleteProduct(manager), server.WithTransaction)
 
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func newLogger() *slog.Logger {
+	var logLevel slog.Level
+
+	switch os.Getenv("ENV") {
+	case "DEV":
+		logLevel = slog.LevelDebug
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true,
+	}))
+
+	slog.SetDefault(logger)
+
+	return logger
+
+}
+
+func newDb() *sql.DB {
+	connStr := os.Getenv("DBSTRING")
+    if connStr == "" {
+        panic("DBSTRING environment variable must be provided to initialize the server")
+    }
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
